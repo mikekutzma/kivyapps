9eef2906f7590e31f46a2fa5a6f88da0238d897a
Mike Kutzma <michaelkutzma@gmail.com>
2017-08-04 07:09:02 -0400

    animate ball

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import NumericProperty, ReferenceListProperty,\
+    ObjectProperty
 from kivy.vector import Vector
+from kivy.clock import Clock
+from random import randint
 
 class PongBall(Widget):
 
@@ -17,9 +20,13 @@ class PongGame(Widget):
 
     ball = ObjectProperty(None)
 
+    def serve_ball(self):
+        self.ball.center = self.center
+        self.ball.velocity = Vector(4,0).rotate(randint(0,360))
+
     def update(self,dt):
         self.ball.move()
-        
+
         #bounce vertical
         if (self.ball.y < 0) or (self.ball.top > self.height):
             self.ball.vely *= -1
@@ -28,11 +35,11 @@ class PongGame(Widget):
         if (self.ball.x < 0) or (self.ball.right > self.width):
             self.ball.velx *= -1
 
-        
 
 class PongApp(App):
     def build(self):
         game = PongGame()
+        game.serve_ball()
         Clock.schedule_interval(game.update,1.0/60.0)
         return game
 
